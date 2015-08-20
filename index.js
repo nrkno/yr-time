2bd0097fd83d38716e4302d21f621e261fea06b2
Sofien Lazreg <Sofien.Lazreg@nrk.no>
2015-08-20 16:40:31 +0200

    Inital

---
+++ index.js
@@ -0,0 +1,142 @@
+'use strict';
+
+var isPlainObject = require('is-plain-obj'),
+    moment = require('moment'),
+    PARSE_KEYS = ['start', 'end', 'middle', 'rise', 'set', 'created', 'update', 'from', 'to'],
+    localNow = Date.now(),
+    utcNow = moment.utc().valueOf();
+
+exports.ANNUALLY = 'annually';
+exports.DAILY = 'daily';
+exports.DAY_END = 18;
+exports.DAY_START = exports.DAY_END - 12;
+exports.HOURLY = 'hourly';
+exports.MONTHLY = 'monthly';
+// Local timezone offset
+exports.TZ_OFFSET = moment().utcOffset();
+exports.WEEKLY = 'weekly';
+
+// Monkey patch Moment for stringify support
+moment.fn.toJSON = function () {
+	return this.format();
+};
+
+/**
+ * 'Now' relative to UTC and localtime
+ * @returns {Number}
+ */
+exports.now = function () {
+	return utcNow + (Date.now() - localNow);
+};
+
+/**
+ * Retrieve epoch from UTC string
+ * @param {Moment|String} date
+ * @returns {Number}
+ */
+exports.epochFromUTC = function (date) {
+	date = moment.isMoment(date) ? date : Date.parse(date);
+	return moment.utc(date).valueOf();
+};
+
+/**
+ * Determine if 'date1' and 'date2' fall within same 'interval'
+ * @param {Moment} date1
+ * @param {Moment} date2
+ * @param {String} interval
+ * @returns {Boolean}
+ */
+exports.sameInterval = function (date1, date2, interval) {
+	switch (interval) {
+		case exports.DAILY:
+			return getDay(date1).isSame(getDay(date2), 'day');
+	}
+};
+
+/**
+ * Retrieve number of days between 'date1' and 'date2'
+ * @param {Moment} date1
+ * @param {Moment} date2
+ * @returns {Number}
+ */
+exports.daysFrom = function (date1, date2) {
+	return getDay(date1).diff(getDay(date2), 'days');
+};
+
+/**
+ * Retrieve number of milliseconds between 'date1' and 'date2'
+ * @param {Moment|String} date1
+ * @param {Moment|String} date2
+ * @returns {Number}
+ */
+exports.millisecondsFrom = function (date1, date2) {
+	date1 = moment.isMoment(date1) ? date1 : Date.parse(date1);
+	date2 = moment.isMoment(date2) ? date2 : Date.parse(date2);
+
+	return moment(date2).diff(moment(date1));
+};
+
+/**
+ * Retrieve long format day text from 'date'
+ * @param {Moment} date
+ * @param {Number} daysFromNow
+ * @param {Object} grammar
+ * @returns {String}
+ */
+exports.formatDay = function (date, daysFromNow, grammar) {
+	if (daysFromNow < 2) {
+		var hour = date.hour();
+
+		return daysFromNow == 1 ? grammar.tomorrow : hour >= exports.DAY_END || hour < exports.DAY_START ? grammar.tonight : grammar.today;
+	}
+
+	return date.format('dddd');
+};
+
+/**
+ * Parse date strings into moment instances
+ * @param {Object} obj
+ */
+exports.parse = function (obj) {
+	function traverse(o) {
+		// Check if is object or array
+		if (!(Array.isArray(o) || isPlainObject(o))) return;
+		for (var prop in o) {
+			// Match prop with keys
+			if (~PARSE_KEYS.indexOf(prop) && ('number' == typeof o[prop] || 'string' == typeof o[prop])) {
+				o[prop] = moment.parseZone(o[prop]);
+			} else {
+				traverse(o[prop]);
+			}
+		}
+	}
+
+	traverse(obj);
+};
+
+/**
+ * Get expiry as epoch timestamp
+ * @param {Number} number
+ * @param {String} key
+ * @returns {String}
+ */
+exports.getExpires = function (number, key) {
+	return moment.utc().add(number, key).valueOf();
+};
+
+/**
+ * Adjust 'date' to whole day, taking into account daily offset start time
+ * @param {Moment} date
+ * @returns {Moment}
+ */
+function getDay(date) {
+	var d = moment({
+		y: date.year(),
+		M: date.month(),
+		d: date.date(),
+		h: 0,
+		m: 0
+	});
+
+	return date.hour() < exports.DAY_START ? d.subtract(1, 'day') : d;
+}
---
+++ test/lib-test.js
@@ -0,0 +1,104 @@
+'use strict';
+
+var moment = require('moment/moment.js')
+
+, date, expect;
+
+// Make it work in node..
+try {
+	date = require('../src/index.js');
+	expect = require('expect.js');
+// .. or browser
+} catch (err) {
+	date = require('src/index.js');
+	expect = window.expect;
+}
+
+describe('date', function () {
+	describe('now()', function () {
+		it('should return correct epoch timestamp within range of an hour', function () {
+			var now = Date.now();
+			expect(date.now()).to.be.within(now, moment(now).add(1, 'hour').valueOf());
+		});
+	});
+	describe('sameInterval()', function () {
+		describe('DAILY', function () {
+			it('should return true for 2 identical days', function () {
+				expect(date.sameInterval(moment('2014-01-01T00:00:00'), moment('2014-01-01T00:00:00'), date.DAILY)).to.be.true;
+			});
+			it('should return true for 2 days falling between 6:00 and 24:00 the same day', function () {
+				expect(date.sameInterval(moment('2014-01-01T23:00:00'), moment('2014-01-01T12:00:00'), date.DAILY)).to.be.true;
+			});
+			it('should return true for 2 days falling between 0:00 and 6:00 the same day', function () {
+				expect(date.sameInterval(moment('2014-01-02T01:00:00'), moment('2014-01-02T02:00:00'), date.DAILY)).to.be.true;
+			});
+			it('should return true for 2 days falling between 6:00 and 5:59 the following day/month/year', function () {
+				expect(date.sameInterval(moment('2014-01-01T05:59:00'), moment('2013-12-31T12:00:00'), date.DAILY)).to.be.true;
+			});
+			it('should return false for 2 days falling on either side of 6:00', function () {
+				expect(date.sameInterval(moment('2014-01-01T05:59:00'), moment('2014-01-01T06:00:00'), date.DAILY)).to.not.be.true;
+			});
+		});
+	});
+
+	describe('daysFrom()', function () {
+		it('should return 0 for 2 identical dates', function () {
+			expect(date.daysFrom(moment('2014-01-01T00:00:00'), moment('2014-01-01T00:00:00'))).to.eql(0);
+		});
+		it('should return 0 for 2 dates falling between 6:00 and 24:00 the same day', function () {
+			expect(date.daysFrom(moment('2014-01-01T23:00:00'), moment('2014-01-01T12:00:00'))).to.eql(0);
+		});
+		it('should return 0 for 2 dates falling between 0:00 and 6:00 the same day', function () {
+			expect(date.daysFrom(moment('2014-01-02T02:00:00'), moment('2014-01-02T01:00:00'))).to.eql(0);
+		});
+		it('should return 0 for 2 dates falling between 6:00 and 5:59 the following day/month/year', function () {
+			expect(date.daysFrom(moment('2014-01-01T05:59:00'), moment('2013-12-31T12:00:00'))).to.eql(0);
+		});
+		it('should return 1 for 2 dates falling on either side of 6:00', function () {
+			expect(date.daysFrom(moment('2014-01-01T06:01:00'), moment('2014-01-01T05:59:00'))).to.eql(1);
+		});
+		it('should return 1 for 2 dates falling at 6:00 one day apart', function () {
+			expect(date.daysFrom(moment('2014-01-02T06:00:00'), moment('2014-01-01T06:00:00'))).to.eql(1);
+		});
+		it('should return a value greater than 1 for 2 dates more than 1 day apart', function () {
+			expect(date.daysFrom(moment('2014-01-04T05:59:00'), moment('2014-01-01T06:01:00'))).to.eql(2);
+		});
+	});
+
+	describe('millisecondsFrom()', function () {
+		it('should return milliseconds between the two dates', function () {
+			expect(date.millisecondsFrom('2014-01-01T00:00:00', '2014-01-01T00:20:00')).to.eql(1200000);
+		});
+		it('should return minus milliseconds between the two dates', function () {
+			expect(date.millisecondsFrom('2014-01-01T00:20:00', '2014-01-01T00:00:00')).to.eql(-1200000);
+		});
+	});
+
+	describe('parse() from epoch to utc', function () {
+		it('should return simple object with parsed moments', function () {
+			var obj = {
+				foo: {},
+				bar: 'bat',
+				start: 1423227600000,
+				end: 1423231200000
+			};
+			date.parse(obj);
+			expect(moment.isMoment(obj.start)).to.be(true);
+			expect(moment.isMoment(obj.end)).to.be(true);
+		});
+		it('should return array containing objects with parsed moments', function () {
+			var array = [{
+					interval: {
+						foo: {},
+						bar: 'bat',
+						start: 1423227600000,
+						end: 1423231200000
+					}
+				}
+			];
+			date.parse(array);
+			expect(moment.isMoment(array[0].interval.start)).to.be(true);
+			expect(moment.isMoment(array[0].interval.end)).to.be(true);
+		});
+	});
+});
