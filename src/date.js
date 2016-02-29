eb14f27092fbbaec838a2d9157dde6ec2739a459
Alexander Pope <alex@pope-industries.com>
2016-02-29 20:59:01 +0100

    sketch api

---
+++ src/date.js
@@ -0,0 +1,59 @@
+'use strict';
+
+class Date {
+  constructor () {
+
+  }
+
+  add () {
+
+  }
+
+  subtract () {
+
+  }
+
+  diff () {
+
+  }
+
+  year () {
+
+  }
+
+  month () {
+
+  }
+
+  date () {
+
+  }
+
+  hour () {
+
+  }
+
+  minute () {
+
+  }
+
+  isSame () {
+
+  }
+
+  isBefore () {
+
+  }
+
+  format () {
+
+  }
+
+  clone () {
+
+  }
+
+  toJSON () {
+
+  }
+}
